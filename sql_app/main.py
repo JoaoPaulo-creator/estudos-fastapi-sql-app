b3ab962b66b2697bae0128b30a37c9fb18b275d7
JoaoPaulo-creator <joaosarto@gmail.com>
2022-10-21 19:34:17 -0300

    adicionado status code nas funções de criação e consulta

--- sql_app/main.py
+++ sql_app/main.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, status
 from sqlalchemy.orm import Session
 
 from . import crud, models, schemas
@@ -23,7 +23,7 @@ como se é utilizado em rotas padrões do FastAPI
 '''
 
 
-@app.post('/users/', response_model=schemas.User)
+@app.post('/users/', response_model=schemas.User, status_code=201)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
@@ -31,13 +31,13 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 
-@app.get('/users/', response_model=list[schemas.User])
+@app.get('/users/', response_model=list[schemas.User], status_code=200)
 def consultar_lista_usuarios(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     user = crud.get_users(db, skip=skip, limit=limit)
     return user
 
 
-@app.get('/users/{user_id}', response_model=schemas.User)
+@app.get('/users/{user_id}', response_model=schemas.User, status_code=200)
 def ler_usuario_por_id(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
@@ -45,12 +45,12 @@ def ler_usuario_por_id(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
-@app.post('/users/{user_id}/items/', response_model=schemas.Item)
+@app.post('/users/{user_id}/items/', response_model=schemas.Item, status_code=201)
 def criar_item_para_usuario(user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)):
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
 
-@app.get('/items/', response_model=list[schemas.Item])
+@app.get('/items/', response_model=list[schemas.Item], status_code=200)
 def ler_itens(skip: int = 0, limit: int = 100, db: SessionLocal = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
